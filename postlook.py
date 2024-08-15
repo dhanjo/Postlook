8064d6e9887d9a09aabd72e66edbff46c9084b2f
Dhananjay <dhananjaygarg192002@gmail.com>
2024-08-15 13:26:45 +0530

    Update postlook.py

--- postlook.py
+++ postlook.py
@@ -1,28 +1,19 @@
 from postman_collection import postman_collections
 from postman_teams import postman_teams
 from postman_requests import postman_requests
-from colorama import init, Fore, Style
 import argparse
 
-init()
-
-parser = argparse.ArgumentParser(description="Postlook is a tool which helps in extracting publicly available information related to organization on postman.")
-parser.add_argument('-q', '--query', metavar='<Organization Name>', type=str, help='Enter the keyword to search for')
+# Setting up argument parsing
+parser = argparse.ArgumentParser(description="Postlook is a tool that helps extract publicly available information related to organizations on Postman.")
+parser.add_argument('-q', '--query', metavar='<Organization Name>', type=str, required=True, help='Enter the keyword to search for')
 parser.add_argument('-o', '--output', metavar='<Output File Name>', type=str, help='Specify the output file name')
 
 args = parser.parse_args()
 query = args.query
 output_file = args.output
 
-if not query and not output_file:
-    parser.print_help()
-    exit()
-    
-if output_file and not query:
-    parser.print_help()
-    exit()
-
-postman_art = '''
+# ASCII art and strings (using raw string to avoid invalid escape sequences)
+postman_art = r'''
     ____             __  __            __  
    / __ \____  _____/ /_/ /___  ____  / /__
   / /_/ / __ \/ ___/ __/ / __ \/ __ \/ //_/
@@ -32,68 +23,61 @@ postman_art = '''
 
 postman_monitoring = "            POSTMAN MONITORING"
 twitter = "    Follow me on Twitter: @dhananjaygarg_"
+
+# Printing output to the console
 print("\n")
-print(Fore.CYAN + Style.BRIGHT + postman_art + Style.RESET_ALL)
-print("\n")
-
-
-print(" " + Fore.YELLOW + postman_monitoring + Style.RESET_ALL)
-
-print(" " + Fore.RED + twitter + Style.RESET_ALL)
+print(postman_art)
 print("\n")
 
-print(Fore.GREEN + " [+] Publicly Exposed - Workspaces and Collections" + Style.RESET_ALL)
+print(" " + postman_monitoring)
+print(" " + twitter)
+print("\n")
+
+print("[+] Publicly Exposed - Workspaces and Collections")
 print("\n")
 collections_output = postman_collections(query)
 print(collections_output)
 
 print("\n")
-print(Fore.GREEN + " [+] Publicly Exposed - Teams" + Style.RESET_ALL)
+print("[+] Publicly Exposed - Teams")
 print("\n")
 teams_output = postman_teams(query)
 print(teams_output)
 
 print("\n")
-print(Fore.GREEN + " [+] Publicly Exposed - API Requests" + Style.RESET_ALL)
+print("[+] Publicly Exposed - API Requests")
 print("\n")
 requests_output = postman_requests(query)
 print(requests_output)
 
-print("\n" + Fore.YELLOW + "  " + "─" * 50 + Style.RESET_ALL + "\n")
+print("\n" + "  " + "─" * 50 + "\n")
 
+# Writing to output file if specified
 if output_file:
-    output = ""
+    with open(output_file, 'w') as file:
+        output = ""
 
-    output += "\n"
-    output += Fore.CYAN + Style.BRIGHT + postman_art + Style.RESET_ALL
-    output += "\n"
+        output += "\n"
+        output += postman_art
+        output += "\n"
 
-    output += " " + Fore.YELLOW + postman_monitoring + Style.RESET_ALL
+        output += " " + postman_monitoring
+        output += "\n"
+        output += " " + twitter
+        output += "\n"
 
-    output += "\n"
+        output += "[+] Publicly Exposed - Workspaces and Collections\n"
+        output += collections_output
+        output += "\n"
 
-    output += " " + Fore.RED + twitter + Style.RESET_ALL
-    output += "\n"
+        output += "[+] Publicly Exposed - Teams\n"
+        output += teams_output
+        output += "\n"
 
-    output += Fore.GREEN + " [+] Publicly Exposed - Workspaces and Collections" + Style.RESET_ALL
-    output += "\n"
+        output += "[+] Publicly Exposed - API Requests\n"
+        output += requests_output
+        output += "\n"
 
-    output += collections_output
-    output += "\n"
+        output += "  " + "─" * 50 + "\n"
 
-    output += Fore.GREEN + " [+] Publicly Exposed - Teams" + Style.RESET_ALL
-    output += "\n"
-
-    output += teams_output
-    output += "\n"
-
-    output += Fore.GREEN + " [+] Publicly Exposed - API Requests" + Style.RESET_ALL
-    output += "\n"
-
-    output += requests_output
-    output += "\n"
-
-    output += Fore.YELLOW + "  " + "─" * 50 + Style.RESET_ALL + "\n"
-
-    with open(output_file, 'w', encoding="utf-8") as file:
         file.write(output)
